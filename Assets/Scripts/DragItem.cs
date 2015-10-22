072e7b0fb6c3045cd9777aeb8a6307bcb78cabb7
unknown <u1014631@eae-w-05.ad.utah.edu>
2015-10-21 20:46:13 -0600

    Added UI elements

--- Assets/Scripts/DragItem.cs
+++ Assets/Scripts/DragItem.cs
@@ -3,17 +3,34 @@ using System.Collections;
 
 public class DragItem : MonoBehaviour {
 
+	public UnityEngine.UI.Text multiplierText;
+	public UnityEngine.UI.Text multplierResultText;
+	public int calories;
+
+	private int multiplier;
+	public int resultCalories;
 	// Use this for initialization
 	void Start () {
-	
+		multiplier = 0;
+		multplierResultText.text = "";
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 
 	void OnMouseDown() {
-		Debug.Log ("You clicked Fish");
+		multiplier++;
+		multiplierText.text = "x " + multiplier;
+		resultCalories = calories * multiplier;
+		multplierResultText.text = " = " + resultCalories;
+	}
+
+	public void reset() {
+		multiplier = 0;
+		multplierResultText.text = "";
+		multiplierText.text = "x 0";
+		resultCalories = 0;
 	}
 }
---
+++ Assets/Scripts/EatClick.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EatClick : MonoBehaviour {
+
+	// Update is called once per frame
+	public UnityEngine.UI.Text totalCaloriesText;
+	public GameObject Fish;
+	private int consumedCalories;
+
+	public void OnEat() {
+		consumedCalories += Fish.GetComponent<DragItem> ().resultCalories;
+		Fish.GetComponent<DragItem> ().reset ();
+		totalCaloriesText.text = "Calories: " + consumedCalories + " / 1000" ;
+	}
+}
