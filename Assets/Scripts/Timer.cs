c2b3c01e75c33c2acd3b771f4781e8f308c52eae
yashasg <yashas.gujjar@gmail.com>
2015-10-24 19:14:37 -0600

    added 2 new items,Pin input Screen and timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+    float fTimer = 10;
+    bool TimerStart;
+    public UnityEngine.UI.Text lTimer;
+    public UnityEngine.UI.Button bPill1;
+    Vector2 boxSpawnPosition;
+
+	// Use this for initialization
+	void Start () {
+        boxSpawnPosition=bPill1.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (fTimer > 0)
+        {
+            fTimer -= Time.deltaTime;
+            lTimer.text = fTimer.ToString("0");
+        }
+        if (fTimer <= 0f)
+        {
+           // Application.LoadLevel()
+        }
+	}
+}
